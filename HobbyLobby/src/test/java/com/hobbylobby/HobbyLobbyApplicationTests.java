7602e187bed46bc5f6eca1308d26307612d25867
Maliha Bakhshi <malihabakhshi01@gmail.com>
2020-10-06 22:32:32 +0530

    initial check-in

---
+++ HobbyLobby/src/test/java/com/hobbylobby/HobbyLobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobbylobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyLobbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
